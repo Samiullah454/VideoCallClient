d74f4721a208e6d6837a940daec149074f2d9ef6
unknown <fawadsami142@gmail.com>
2025-07-01 09:05:09 -0700

    Changes

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:44388/api',
-  signalrHubUrl: 'https://localhost:44388/callhub'
+  production: true,
+  apiUrl: 'https://4163-2407-d000-1a-bcdb-e082-c568-ab84-d166.ngrok-free.app/api',
+  signalrHubUrl: 'https://4163-2407-d000-1a-bcdb-e082-c568-ab84-d166.ngrok-free.app/callhub'
 };
