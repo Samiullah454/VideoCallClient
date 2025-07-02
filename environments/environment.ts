a9382d6f93cb756b75667984719bc3d08a50c164
unknown <fawadsami142@gmail.com>
2025-07-03 01:14:27 +0500

    Your commit message here

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://06a7-2407-d000-1a-bfe0-f97b-644a-1165-3bed.ngrok-free.app',
-  signalrHubUrl: 'https://06a7-2407-d000-1a-bfe0-f97b-644a-1165-3bed.ngrok-free.app/callhub'
+  apiUrl: 'https://9b6e-2407-d000-1a-cc31-acd5-c46b-7a0-88ac.ngrok-free.app',
+  signalrHubUrl: 'https://9b6e-2407-d000-1a-cc31-acd5-c46b-7a0-88ac.ngrok-free.app/callhub'
 };
