a95ba82c25566e3d2717db18ba8c5548830ac64a
unknown <fawadsami142@gmail.com>
2025-07-02 07:34:34 +0500

    Changes

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,40 +1,59 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { NgFor, NgIf } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [NgIf,NgFor, MatProgressSpinnerModule 
-    
-  ],
   templateUrl: './join.component.html',
   styleUrl: './join.component.scss'
 })
-export class JoinComponent implements OnInit{
-showLoader = true;
+export class JoinComponent implements OnInit {
+  showLoader = true;
   error!: string;
   roomId!: string;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    if (!isPlatformBrowser(this.platformId)) {
+      console.log('[JoinComponent] Not running in browser, skipping execution.');
+      return;
+    }
+
     const roomId = this.route.snapshot.paramMap.get('roomId');
     const token = this.route.snapshot.queryParamMap.get('token');
-    this.http.get<any>(`${environment.apiUrl}/call/validate-join?roomId=${roomId}&token=${token}`)
-      .subscribe({
-        next: res => {
-          this.roomId = res.roomId;
-          this.router.navigate(['/call', this.roomId]);
-        },
-        error: _ => {
-          this.error = 'Invalid or expired link.';
-          this.showLoader = false;
-        }
-      });
-  }
 
+    console.log('[JoinComponent] URL Params -> roomId:', roomId, ', token:', token);
+
+    if (!roomId || !token) {
+      this.error = 'Invalid link. Missing room ID or token.';
+      this.showLoader = false;
+      return;
+    }
+
+    const url = `${environment.apiUrl}/call/validate-join?roomId=${roomId}&token=${token}`;
+    console.log('[JoinComponent] Calling API:', url);
+
+    this.http.get<any>(url).subscribe({
+      next: res => {
+        console.log('[JoinComponent] Validation success:', res);
+        this.roomId = res.roomId;
+        this.router.navigate(['/call', this.roomId]);
+      },
+      error: err => {
+        console.error('[JoinComponent] Validation error:', err);
+        this.error = 'Invalid or expired link.';
+        this.showLoader = false;
+      }
+    });
+  }
 }
